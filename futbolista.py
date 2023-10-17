5f625d65ebb92974e89cc1c8f028f16374c185fb
Criiper <perezloperacristiandavid@gmail.com>
2023-10-17 00:07:46 -0500

    Commit de más :heavy_plus_sign: :
    Para especificar que lo que estaba dando problemas era en la instanciacion definir deporte como futbol, que habia hacerlo tambien por medio de un set. Y finalmente un bug menor en el __str__

--- futbolista.py
+++ futbolista.py
@@ -43,3 +43,4 @@ class Futbolista(Persona, Deportista):
     
     def __str__(self):
         return (f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte")
+
